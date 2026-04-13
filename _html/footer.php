21587ce25dfd7b574f22e43386469b6f720f2ef5
raaulrodriguees <clovisdavildo@gmail.com>
2026-04-13 17:51:34 -0300

    ajuste raul 8

--- _html/footer.php
+++ _html/footer.php
@@ -3,34 +3,46 @@
             <div class="container">
                 <div class="row justify-content-between align-items-start">
                     <div class="col-12 col-lg-3 text-center d-flex flex-column align-items-center">
-                        <img src="img/logo-pinheiroGui-white.svg" alt="">
+                        <img src="img/logo-pinheiroGui-white.svg" alt="" class="mb-3">
                     </div>
-                    <div class="col-12 d-flex justify-content-center">
-                        <a href="#" class="social-btn text-white"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="social-btn text-white"><i class="bi bi-linkedin"></i></a>
+                    <div class="col-12 d-flex justify-content-center mt-2 mt-lg-0">
+                        <a href="#" class="social-btn text-white mx-2"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="social-btn text-white mx-2"><i class="bi bi-linkedin"></i></a>
                     </div>
                 </div>
+                
                 <div class="row justify-content-between">
-                    <div class="col-12 col-lg-3 text-white">
+                    
+                    <div class="col-12 col-lg-3 text-white text-center text-lg-start">
                         <div>
                             <h6>Rio de Janeiro</h6>
                             <p>Av. Rio Branco 181, 27º Andar<br>20040-918 – Rio de Janeiro – RJ – Brasil</p>
-                            <a href="tel:+552145015000" class="contact-link text-white"><i class="bi bi-telephone"></i> Tel.: +55 (21) 4501 5000</a>
-                            <a href="mailto:pgrj@pinheiroguimaraes.com.br" class="contact-link text-white"><i class="bi bi-envelope"></i> pgrj@pinheiroguimaraes.com.br</a>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="tel:+552145015000" class="contact-link text-white"><i class="bi bi-telephone"></i> Tel.: +55 (21) 4501 5000</a>
+                            </div>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="mailto:pgrj@pinheiroguimaraes.com.br" class="contact-link text-white"><i class="bi bi-envelope"></i> pgrj@pinheiroguimaraes.com.br</a>
+                            </div>
                         </div>
                     </div>
-                     <div class="col-12 col-lg-3 d-flex align-items-center flex-column text-white">
+                    
+                    <div class="col-12 col-lg-3 d-flex flex-column text-white text-center text-lg-start">
                         <div>
                             <h6>São Paulo</h6>
                             <p>Av. Brig. Faria Lima 3064, 14º Andar<br>01451-000 – São Paulo – SP – Brasil</p>
-                            <a href="tel:+551145015000" class="contact-link text-white"><i class="bi bi-telephone"></i> Tel.: +55 (11) 4501 5000</a>
-                            <a href="mailto:pgsp@pinheiroguimaraes.com.br" class="contact-link text-white"><i class="bi bi-envelope"></i> pgsp@pinheiroguimaraes.com.br</a>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="tel:+551145015000" class="contact-link text-white"><i class="bi bi-telephone"></i> Tel.: +55 (11) 4501 5000</a>
+                            </div>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="mailto:pgsp@pinheiroguimaraes.com.br" class="contact-link text-white"><i class="bi bi-envelope"></i> pgsp@pinheiroguimaraes.com.br</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-end text-white">
+                    
+                    <div class="col-12 col-lg-3 d-flex flex-column justify-content-center align-items-center align-items-lg-end text-white text-center text-lg-start">
                         <div>
                             <h6>Selos de certificação</h6>
-                            <div class="d-flex">
+                            <div class="d-flex justify-content-center justify-content-lg-start">
                                 <a href="#" class="footer-award">
                                     <img src="img/selo-iso-iec-27001.png" alt="">
                                 </a>
@@ -40,6 +52,7 @@
                             </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
